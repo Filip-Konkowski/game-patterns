b21e0b1443c881a2918fbefa6bfe221a1753fb1d
filip <fkonkowski@viprsolutions.com>
2018-06-17 20:20:12 +0100

    Install PhpSpec

---
+++ src/App/Notification/Message.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 17/06/18
+ * Time: 14:55
+ */
+
+namespace App\Notification;
+
+
+class Message
+{
+
+}
---
+++ src/App/Notification/NotificationInterface/Listener.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: filip
+ * Date: 17/06/18
+ * Time: 13:59
+ */
+
+namespace App\Notification\NotificationInterface;
+
+
+interface Notification
+{
+    public function update();
+
+}
